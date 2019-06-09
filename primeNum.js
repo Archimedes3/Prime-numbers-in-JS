ffe41ee916acefd94561564579438d5a72f4c634
Archimedes3 <41710441+Archimedes3@users.noreply.github.com>
2019-06-09 03:18:21 +0300

    Create primeNum.js

---
+++ primeNum.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Ctesibius Machine</title>
+</head>
+<body>
+<script>
+var i;
+var values = [];
+values[0] = 2;
+var n = prompt("Give n");
+for (i = 3; i < n; i++)
+{
+	var count = 0;
+	for (j = 2; j < i; j++)
+	{
+		if (i%j != 0)
+		{
+			count = count + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (count == i - 2)
+	{
+		values.push(i);
+	}
+}
+document.write(values);
+</script>
+</body>
+</html>
